41a5edf71b244d01111b322633e9e13ee0939d69
unknown <matej.pavic92@gmail.com>
2021-02-17 14:23:45 +0100

    styling detail-component

--- src/components/location_detail/LocationDetail.jsx
+++ src/components/location_detail/LocationDetail.jsx
@@ -5,18 +5,36 @@ function LocationDetail({setSelectedLocation, selectedLocation}) {
     return (
         <div className='details'>
             {/* <div className='details__heading'> */}
-                <div className='details__card'></div>
+                <div className='details__card'>
+                    <div className='card-wrap'>
+                        <h2>{selectedLocation.name}</h2>
+                        <div>{selectedLocation.address}</div>
+                        <div>{selectedLocation.city}</div>
+                        <button>Request your appointment</button>
+                    </div>
+                </div>
                 <div className='details__image' style={{backgroundImage: `url(${selectedLocation.imageUrl})`}}/>
             {/* </div> */}
             {/* <div className='details__info'> */}
                 {/* <div className='details__contacts'> */}
-                    <div className='details__about'></div>
-                    <div className='details__contact-info'></div>
+                    <div className='details__about'>
+                        <h3>About us</h3>
+                        <div>{selectedLocation.description}</div>
+                    </div>
+                    <div className='details__contact-info'>
+                        <h3>Contact</h3>
+                        <div><span style={{fontWeight: 'bold'}}>Email: </span> <span>{selectedLocation.email}</span></div>
+                        <div><span style={{fontWeight: 'bold'}}>Cell: </span><span>{selectedLocation.phone}</span></div>
+                    </div>
                 {/* </div> */}
-                <div className='details__working-hours'></div>
+                <div className='details__working-hours'>
+                    <h3>Working hours</h3>
+                </div>
             {/* </div> */}
             <div className='details__links'></div>
-            <div className='details__team'></div>
+            <div className='details__team'>
+                <h3>Our team</h3>
+            </div>
             {/* <div>
                 {selectedLocation.name}
             </div>
