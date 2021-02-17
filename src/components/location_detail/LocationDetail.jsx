21e3c93a9bd64e262ac17d8c364a648f4b697e4a
unknown <matej.pavic92@gmail.com>
2021-02-17 12:52:39 +0100

    setting grid in detail-component

--- src/components/location_detail/LocationDetail.jsx
+++ src/components/location_detail/LocationDetail.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import './LocationDetail.scss'
+import './LocationDetail.scss';
 
 function LocationDetail({setSelectedLocation, selectedLocation}) {
     return (
-        <div>
-        
-            <div>
+        <div className='details'>
+            {/* <div className='details__heading'> */}
+                <div className='details__card'></div>
+                <div className='details__image' style={{backgroundImage: `url(${selectedLocation.imageUrl})`}}/>
+            {/* </div> */}
+            {/* <div className='details__info'> */}
+                {/* <div className='details__contacts'> */}
+                    <div className='details__about'></div>
+                    <div className='details__contact-info'></div>
+                {/* </div> */}
+                <div className='details__working-hours'></div>
+            {/* </div> */}
+            <div className='details__links'></div>
+            <div className='details__team'></div>
+            {/* <div>
                 {selectedLocation.name}
             </div>
-            
             {
                 <div onClick={()=>setSelectedLocation(null)}>{"<- BACK"}</div>
-            }
+            } */}
         </div>
     );
 }
-
 export default LocationDetail;
