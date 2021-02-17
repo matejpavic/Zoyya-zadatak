8fc408466a188e20b7754d410241b5986b458390
unknown <matej.pavic92@gmail.com>
2021-02-17 09:59:25 +0100

    adding more styles for location-item component

--- src/components/location_list/LocationList.jsx
+++ src/components/location_list/LocationList.jsx
@@ -5,23 +5,26 @@ import LocationItem from '../location_item/LocationItem';
 
 function LocationList({selectedLocation, setSelectedLocation, locations}) {
     return (
-        <div className='wrapper'>
-            {
-                locations.map(location => {
-                    return (
-                        <div onClick={()=>setSelectedLocation(location)}>
-                            <LocationItem
-                            name={location.name}
-                            imageUrl={location.imageUrl}
-                            category={location.category}
-                            address = {location.address}
-                            selectedLocation={selectedLocation}
-                            />                
-                        </div>
+        <div className='container'>
+            <h1>Featured</h1>
+            <div className='wrapper'>
+                {
+                    locations.map(location => {
+                        return (
+                            <div onClick={()=>setSelectedLocation(location)}>
+                                <LocationItem
+                                name={location.name}
+                                imageUrl={location.imageUrl}
+                                category={location.category}
+                                address = {location.address}
+                                selectedLocation={selectedLocation}
+                                />                
+                            </div>
+                        )
+                    }
                     )
                 }
-                )
-            }
+            </div>
         </div>
     );
 }
